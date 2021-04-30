68f162e13c83b0dc48547537f08fccb364ad7b6f
Thierry Chappuis <tchappui@gmail.com>
2021-04-30 09:41:04 +0200

    Updated settings.py

--- myreactor/settings.py
+++ myreactor/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,5 +126,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-django_heroku.settings(locals())
