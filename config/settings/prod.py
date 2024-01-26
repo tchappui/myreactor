c20d2dee171583be39f2d00ed47c16817eff0202
tchappui <tchappui@gmail.com>
2024-01-26 16:29:35 +0100

    Added sentry

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *
 from .base import env
 
-import pymysql
-
 DEBUG = False
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
@@ -12,8 +10,6 @@ DATABASES = {
 DATABASES["default"]["CONN_MAX_AGE"] = env.int(
     "CONN_MAX_AGE", default=60
 )  # noqa: F405
-
-pymysql.install_as_MySQLdb()
 
 # SECURITY
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -27,4 +23,18 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
 SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=True)
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     "SECURE_CONTENT_TYPE_NOSNIFF", default=True
+)
+
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://2edc96be82cc85cd69ffb8efd8f981b6@o162891.ingest.sentry.io/4506638271709184",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
 )
