2301d29dab44c749efa0de5b0bee4b508d2ed24e
Thierry Chappuis <tchappui@users.noreply.github.com>
2025-11-13 08:56:59 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "myreactor.geniechimique.ch"]
 
 
 # Application definition
